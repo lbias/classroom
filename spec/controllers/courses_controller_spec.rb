d98ce91d12332172ec3c276357c35c1768be5447
lbias <306508354@qq.com>
2017-07-24 17:04:55 +0800

    test and implement courses#edit

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -83,4 +83,18 @@ RSpec.describe CoursesController do
     end
   end
 
+  describe "GET edit" do
+    it "assign course" do
+      course = create(:course)
+      get :edit, params: { id: course.id }
+      expect(assigns[:course]).to eq(course)
+    end
+
+    it "render template" do
+      course = create(:course)
+      get :edit, params: { id: course.id }
+      expect(response).to render_template("edit")
+    end
+  end
+
 end
