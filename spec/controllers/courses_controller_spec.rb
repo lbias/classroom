fc0aa8aa5d06f6fae3ed5bf8b953a71e67c7b95e
lbias <306508354@qq.com>
2017-07-24 19:21:23 +0800

    test and implement courses#destroy

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -123,13 +123,32 @@ RSpec.describe CoursesController do
       it "doesn't update a record" do
         course = create(:course)
         put :update, params: {id: course.id, course: {title: "", description: "Description"}}
-        expect(course.description).not_to eq("Description")
+        expect(response).not_to redirect_to course_path(course)
       end
 
       it "renders edit template" do
         course = create(:course)
         put :update, params: { id: course.id, course: {title: "", description: "Description"}}
         expect(response).to render_template("edit")
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "assigns @course" do
+        course = create(:course)
+        delete :destroy, params: { id: course.id }
+        expect(assigns[:course]).to eq(course)
+      end
+
+      it "deletes a record" do
+        course = create(:course)
+        expect{delete :destroy, params: { id: course.id }}.to change(Course, :count).by(-1)
+      end
+
+      it "redirects to courses_path" do
+        course = create(:course)
+        delete :destroy, params: { id: course.id }
+        expect(response).to redirect_to courses_path
       end
     end
   end
